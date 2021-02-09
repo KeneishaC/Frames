c2236db8c27fdecf052578b48aab0c66dff57f44
keneishac <keneishacorbett@gmail.com>
2021-02-09 12:10:19 -0800

    Navbar added and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import Frames from './Components/Frames/Frames'
+// import Frames from './Components/Frames/Frames'
+// import Navbar from './Components/Navbar/Navbar'
 
+import { Frames, Navbar} from './Components'
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             <Frames />
         </div>
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Typography } from '@material-ui/core'
+import { ShoppingCart } from "@material-ui/icons"
 
-const NavBar = () => {
+import logo from '../../assets/glasses-frame.png'
+import useStyles from './styles'
+
+const Navbar = () => {
+    const classes = useStyles()
     return (
         <div>
-
+            <AppBar position='fixed' className={classes.appBar} color='inherit'>
+                <Toolbar>
+                    <Typography variant='h6' className={classes.title} color='inherit'>
+                        <img src={logo} alt='Frames' height='25px' className={classes.image}/>
+                        Frames
+                    </Typography>
+                    <div className={classes.grow}/>
+                    <div className={classes.button}>
+                        <IconButton aria-label='Show Cart items' color="inherit">
+                            <Badge badgeContent={2} color='secondary'></Badge>
+                            <ShoppingCart />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
