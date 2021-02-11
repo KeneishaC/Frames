8058d29800c2f5b8e4bab47036689b8361ac2375
keneishac <keneishacorbett@gmail.com>
2021-02-10 21:37:13 -0800

    Updates Readme.md

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Conatiner, Typography, Button, Grid } from "@material-ui/core"
-import useStyles from './styles';
+import { Container, Typography, Button, Grid } from "@material-ui/core"
+import useStyles from './styles'
 
 const Cart = ({ cart }) => {
-    const classes = useStyles();
-
-    const isEmpty = !cart.line_item.length
+    const classes = useStyles()
+    
+    const isEmpty = !cart.line_items.length
 
     const EmptyCart = () => (
         <Typography variant='subtitle1'>You don't have any frames, start adding them now!</Typography>
@@ -13,7 +13,7 @@ const Cart = ({ cart }) => {
     const FilledCart = () => (
         <>
             <Grid container spacing={3}>
-                {cart.line_items.map(() => (
+                {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
                         <div>
                             {item.name}
@@ -26,13 +26,12 @@ const Cart = ({ cart }) => {
                             Subtotal: { cart.subtotal.formatted_with_symbol }
                     </Typography>
                     <div>
-                        <Button className={classes.emptyButton} size={large} type={button} variant='contained' color='secondary'>Empty Cart</Button>
-                        <Button className={classes.checkoutButton} size={large} type={button} variant='contained' color='primary'>Checkout</Button>
+                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                        <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
                     </div>
             </div>
         </>
     )
-
     return(
         <Container>
             <div className={classes.toolbar}/>
