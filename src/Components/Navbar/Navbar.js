7439323c223cc55009bb7caf9132ed29a0a0f2d8
keneishac <keneishacorbett@gmail.com>
2021-02-11 10:15:35 -0800

    adds link for home and shopping cart

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link  } from 'react-router-dom'
 import {AppBar, Toolbar, IconButton, Badge, MenuItem, Typography } from '@material-ui/core'
 import { ShoppingCart } from "@material-ui/icons"
 
@@ -11,13 +12,13 @@ const Navbar = ( {totalItems} ) => {
         <div>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title} color='inherit'>
+                    <Typography component={ Link } to='/ 'variant='h6' className={classes.title} color='inherit'>
                         <img src={logo} alt='Frames' height='25px' className={classes.image}/>
                         Frames
                     </Typography>
                     <div className={classes.grow}/>
                     <div className={classes.button}>
-                        <IconButton aria-label='Show Cart items' color="inherit">
+                        <IconButton component={ Link } to='/cart' aria-label='Show Cart items' color="inherit">
                             <Badge badgeContent={totalItems} color='secondary'></Badge>
                             <ShoppingCart />
                         </IconButton>
