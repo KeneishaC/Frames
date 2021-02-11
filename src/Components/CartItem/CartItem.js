09d2d944fdba539db777455b97cd371dcba2fa36
keneishac <keneishacorbett@gmail.com>
2021-02-11 09:40:45 -0800

    adds initial set up and styling for cart items

---
+++ src/Components/CartItem/CartItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Typograpgy, Buttton, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+
+import useStyles from './styles.js'
+
+const CartItem = () => {
+    
+    const 
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CartItem
