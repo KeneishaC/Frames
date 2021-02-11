5ab974014fafda428569e5c627a4d3c40d4a3855
keneishac <keneishacorbett@gmail.com>
2021-02-11 09:46:03 -0800

    imports CartItems

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from "@material-ui/core"
+import CartItem from '../CartItem/CartItem'
+
 import useStyles from './styles'
 
 const Cart = ({ cart }) => {
     const classes = useStyles()
     
-    const isEmpty = !cart.line_items.length
-
+ 
     const EmptyCart = () => (
         <Typography variant='subtitle1'>You don't have any frames, start adding them now!</Typography>
     )
@@ -15,7 +16,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem />
                     </Grid>
                 ))}
             </Grid>
@@ -30,11 +31,13 @@ const Cart = ({ cart }) => {
             </div>
         </>
     )
+
+    if(!cart.line_items) return 'loading...'
     return(
         <Container>
             <div className={classes.toolbar}/>
             <Typography className={classes.title} variant='h3'>Your frames shopping Cart</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart /> }
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
 
