4ef2fc669496fb792d61257b49265affe832ffb5
keneishac <keneishacorbett@gmail.com>
2021-02-10 17:05:22 -0800

    adds commerce.js key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { commerce } from './lib/commerce'
 // import Frames from './Components/Frames/Frames'
 // import Navbar from './Components/Navbar/Navbar'
 
 import { Frames, Navbar} from './Components'
 
 const App = () => {
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        const {data} = await commerce.products.list()
+
+        setProducts(data)
+    }
+
+    useEffect(() => {
+        fetchProducts()
+
+    },[])
     return (
-        <div>
+        <div> 
             <Navbar />
             <Frames />
         </div>
--- src/Components/Frames/Frames.js
+++ src/Components/Frames/Frames.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {Grid} from  '@material-ui/core'
 
 import Frame from '../Frame/Frame'
-
+import useStyles from './styles'
 const frames = [
     { id: 1, name: 'cat-eye', description: 'cat-eye glasses', price: '$150', image: 'https://image.ebdcdn.com/image/upload/c_fill,e_sharpen:70,f_auto,h_400,q_85,w_800/v1/product/frame/gray/pl7306_0.jpg'},
     { id: 2, name: 'round', description: 'round glasses', price: '$150', image: 'https://cdn.shopify.com/s/files/1/0081/4075/0948/products/LF32AC3OPT_2_720x.jpg?v=1574436476'}
 ]
 
 const Frames = () => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify='center' spacing={4}>
                 {frames.map((frame) => (
                     <Grid item key={frames.id} xs={12} sm={6}md={4} lg={3}>
