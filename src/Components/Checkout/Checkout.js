3c86ae1620a80d797a0a72a4771b698f07402081
keneishac <keneishacorbett@gmail.com>
2021-02-12 11:15:40 -0800

    adds setup for checkout form

---
+++ src/Components/Checkout/Checkout.js
@@ -0,0 +1,36 @@
+import React, {useState } from 'react'
+import {Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'
+import useStyles from './styles';
+
+
+const steps = ['Shipping adress', 'Payment Details']
+
+
+const Checkout = () => {
+    const classes = useStyles();
+
+    const [activeStep, setActiveStep] = useState(0)
+
+    return (
+        <>
+            <div className={classes.toolbar} />
+                <main className={classes.layout}>
+                    <Paper className={classes.paper}>
+                        <Typography variant='h4' align='center'>Checkout</Typography>
+                        <Stepper activeStep={0} className={classes.stepper}>
+                            {steps.map((step) =>
+                                <Step>
+                                    <StepLabel>{step}</StepLabel>
+                                </Step>
+                            )}
+                        </Stepper>
+                    </Paper> 
+                </main>
+
+
+
+        </>
+    )
+}
+
+export default Checkout
