7a7c394194f3c55274804fdac2563874bdf60a03
keneishac <keneishacorbett@gmail.com>
2021-02-13 10:28:00 -0800

    adds setup for Address from

--- src/Components/CheckoutForm/AddressForm.js
+++ src/Components/CheckoutForm/AddressForm.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { InputLabel, Select, MennuItem, Grid, Typography } from '@material-ui/core'
+import { useForm, FormProvider } from 'react-hook-form'
 
 const AddressForm = () => {
+    const methods = useForm()
+
     return (
-        <div>
-            Address Form
-        </div>
+        <>
+            <Typography variant='h6' gutterBottom> Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form>
+                    <Grid container spacing={3}>
+
+                    </Grid>
+                </form>
+            </FormProvider>
+            
+        </>
     )
 }
 
