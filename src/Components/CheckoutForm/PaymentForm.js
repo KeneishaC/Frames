88d50a6862ff2308d3301fbb2297af9ddf8c77a4
keneishac <keneishacorbett@gmail.com>
2021-02-16 16:09:18 -0800

    adds stripe promise for checkout

--- src/Components/CheckoutForm/PaymentForm.js
+++ src/Components/CheckoutForm/PaymentForm.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Typography, Button, Divider } from '@material-ui/core'
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import  Review from './Review'
 
-const PaymentForm = () => {
+const stripePromise = loadStripe('...')
+
+const PaymentForm = ({ checkoutToken }) => {
+
     return (
-        <div>
-            Payment Form
-        </div>
+        <>
+            <Review checkoutToken={checkoutToken} />
+            <Divider />
+            <Typography variant='h6' gutterBottom style={{margin: '20px 0'}}>Payment Method</Typography>
+            <Elements stripe={stripePromise}>
+
+            </Elements>
+        </>
     )
 }
 
