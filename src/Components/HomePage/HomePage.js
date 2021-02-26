db8c1cb2d889987a1f23a34d1ab1aefc5250287b
keneishac <keneishacorbett@gmail.com>
2021-02-26 12:32:25 -0800

    adds classes for styling

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,10 +4,12 @@ import { Link, useLocation  } from 'react-router-dom'
 import useStyles from './styles'
 
 const HomePage = () => {
+    const classes = useStyles()
+
     return (
         <div>
             <Grid item xs={12} sm={6}>
-                <Typography variant='h1' gutterBottom>Welcome to Frames</Typography>
+                <Typography variant='h1' gutterBottom >Welcome to Frames</Typography>
                 <Button component={Link} to='/shop' size='large' variant='conatined' type='button' color='primary'>Start Shopping</Button>
             </Grid>
         </div>
