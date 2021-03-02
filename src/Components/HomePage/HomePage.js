9a1753f3a3333e3ae2b1d19df9db8eda2004e05a
keneishac <keneishacorbett@gmail.com>
2021-03-01 16:05:42 -0800

    makes changes to greeting on home page

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -8,9 +8,10 @@ const HomePage = () => {
 
     return (
         <div className={classes.root}>
-            <Grid  item xs={12} sm={6}>
+            <Grid  item xs={12}>
                 <Typography className={classes.media} variant='h1' color='inherit' gutterBottom >Frames Eyeware</Typography>
-                <Button className={classes.button} component={Link} to='/shop' size='small' variant='conatined' type='button' color='outlined'>Start Shopping</Button>
+                <div />
+                <Button className={classes.button} component={Link} to='/shop' size='large' color='outlined'>Start Shopping</Button>
             </Grid>
         </div>
     )
